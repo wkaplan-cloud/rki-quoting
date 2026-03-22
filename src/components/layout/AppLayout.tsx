8222facd00cbca627ad35e1a55b364213d508289
Warren Kaplan <warrenkaplan@Warrens-MacBook-Pro.local>
2026-03-22 11:00:27 +0200

    Fix main layout overflow to contain kanban width

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,7 +4,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-[#F5F2EC]">
       <Sidebar />
-      <main className="ml-56 flex-1 flex flex-col min-h-screen">
+      <main className="ml-56 flex-1 flex flex-col min-h-screen min-w-0 overflow-x-hidden">
         {children}
       </main>
     </div>
